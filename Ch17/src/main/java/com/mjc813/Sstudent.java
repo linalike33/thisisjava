6836e5fa0f9f507e5121af0e81c13aa86aa751c9
nahyun <linalike33@gmail.com>
2026-03-05 21:19:51 +0900

    다햇다!

---
+++ Ch17/src/main/java/com/mjc813/Sstudent.java
@@ -0,0 +1,13 @@
+package com.mjc813;
+
+public class Sstudent {
+    private String name;
+    private int score;
+
+    public Sstudent(String name ,int score){
+        this.name = name;
+        this.score = score;
+    }
+    public  String getName(){return name;}
+    public int getScore(){return  score;}
+}
---
+++ Ch17/src/main/java/com/mjc813/eotjfs.java
@@ -0,0 +1,16 @@
+package com.mjc813;
+
+public class eotjfs {
+    private String name;
+    private String sex;
+    private int score;
+
+    public eotjfs(String name,String sex, int score){
+        this.name = name;
+        this.sex = sex;
+        this.score = score;
+    }
+    public String getName(){return name;}
+    public String getSex(){return  sex;}
+    public int getScore(){return score;}
+}
