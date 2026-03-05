0429bb5ddc5c94441efa32d4d05fbfe9b768c2c6
nahyun <linalike33@gmail.com>
2026-03-05 21:20:01 +0900

    수정

--- Ch17/src/main/java/com/mjc813/Ch17.java
+++ Ch17/src/main/java/com/mjc813/Ch17.java
@@ -8,14 +8,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.mjc813.Strean.sum;
 
 public class Ch17 {
+    public Ch17() {
+    }
+
     //스트림
-    public void ster(){
+    public void ster() {
         Set<String> set = new HashSet<>();
         set.add("홍길동");
         set.add("집갈래");
@@ -24,17 +28,18 @@ public class Ch17 {
         Stream<String> stream = set.stream();
         stream.forEach(name -> System.out.println(name));
     }
+
     //내부반복자
-    public void Paralle(){
-        List<String>list = new ArrayList<>();
+    public void Paralle() {
+        List<String> list = new ArrayList<>();
         list.add("홍길동");
         list.add("신용권");
         list.add("람다식");
         list.add("집가자");
 
-        Stream<String>parllelStream = list.parallelStream(); //parallelStream 하나의 덩어리의 리스트를 여러개의 작은 조작으로 나누는것
-        parllelStream.forEach(name->{
-            System.out.println(name +":"+Thread.currentThread().getName());
+        Stream<String> parllelStream = list.parallelStream(); //parallelStream 하나의 덩어리의 리스트를 여러개의 작은 조작으로 나누는것
+        parllelStream.forEach(name -> {
+            System.out.println(name + ":" + Thread.currentThread().getName());
             //Thread 하나의 일꾼, 주체를 말한다 지금일을 하는 사람
             //urrentThread() 지금 현재 이 코드에서 실행을 하고 있는 일꾼을 지목한다
             //getName 그 일꾼의 이름이 뭐냐고 물어보는 코드이다
@@ -42,12 +47,13 @@ public class Ch17 {
             //따라서 하나의 리스트를 나눠서 하나하나 처리를 한다음 젤먼저 일을 끝내는 사람위주로 실행된다
         });
     }
+
     //중간 처리와 최종처리
-    public void Stert(){
-        List<Student>list = Arrays.asList( //리스트로집어넣고싶을때
-                new Student("홍길동",10),
-                new Student("신유권",10),
-                new Student("미권죵",30)
+    public void Stert() {
+        List<Student> list = Arrays.asList( //리스트로집어넣고싶을때
+                new Student("홍길동", 10),
+                new Student("신유권", 10),
+                new Student("미권죵", 30)
 
         );
 
@@ -56,47 +62,52 @@ public class Ch17 {
                 .average() //숫자를 다더하고 개수로 나누ㅗ라
                 .getAsDouble(); //숫자로 꺼냄
 
-        System.out.println("평균점수:"+avg);
+        System.out.println("평균점수:" + avg);
     }
+
     //리소스로부터 스트림얻기(컬렉션으로부터 스트림 얻기
-    public void Stertt(){
+    public void Stertt() {
         List<Product> list = new ArrayList<>();
-        for(int i =1; i<=5; i++){
-            Product product = new Product(i,"상품"+i,"회사",(int)
-                    (1000*Math.random()));
+        for (int i = 1; i <= 5; i++) {
+            Product product = new Product(i, "상품" + i, "회사", (int)
+                    (1000 * Math.random()));
             list.add(product);
         }
         Stream<Product> stream = list.stream();
-        stream.forEach(p->System.out.println(p));
+        stream.forEach(p -> System.out.println(p));
 
     }
+
     //배열로부터 스트림얻기
-    public void Stgfdg(){
-        String[] strArray = {"홍길동","신용권","김미나"};
+    public void Stgfdg() {
+        String[] strArray = {"홍길동", "신용권", "김미나"};
         Stream<String> stream = Arrays.stream(strArray); //멈춰 있는 배열을 흐르게(Stream) 만들어라! Stream<String 글자가 지나가는 일반 벨트.
-        stream.forEach(item->System.out.println(item+","));
+        stream.forEach(item -> System.out.println(item + ","));
         System.out.println();
 
-        int[] intArray = {1,2,3,4,5};
+        int[] intArray = {1, 2, 3, 4, 5};
         IntStream intStream = Arrays.stream(intArray);
-        intStream.forEach(item-> System.out.println(item+","));
+        intStream.forEach(item -> System.out.println(item + ","));
         System.out.println();
     }
+
     //숫자범위로부터 스트림얻기
-    public void exs(){
-       IntStream stream = IntStream.rangeClosed(1,100);
-       stream.forEach(a->sum+=a);
-       System.out.println("총합: "+sum);
+    public void exs() {
+        IntStream stream = IntStream.rangeClosed(1, 100);
+        stream.forEach(a -> sum += a);
+        System.out.println("총합: " + sum);
     }
+
     //파일로부터 스트림 얻기
-    public void Sterfd() throws Exception{ //Exception 예외걍던지기
+    public void Sterfd() throws Exception { //Exception 예외걍던지기
         Path path = Paths.get(Ch17.class.getResource("data.txt").toURI()); //s가 붙은 건 내가 뭘 담는 '그릇'이 아니라, 그릇을 닦거나 음식을 담을 때 쓰는 '도구(집게, 가위)'다!"
-        Stream<String>stream = Files.lines(path, Charset.defaultCharset());
-        stream.forEach(line-> System.out.println(line)); //forEach 하나하나씩 꺼내서 원하는 작업을 시키는ㄴ
+        Stream<String> stream = Files.lines(path, Charset.defaultCharset());
+        stream.forEach(line -> System.out.println(line)); //forEach 하나하나씩 꺼내서 원하는 작업을 시키는ㄴ
         stream.close();
     }
+
     //요소걸러내기(필터링)
-    public void Filter(){
+    public void Filter() {
         List<String> list = new ArrayList<>();
         list.add("홍길동");
         list.add("신용권");
@@ -109,59 +120,62 @@ public class Ch17 {
         System.out.println();
 
         list.stream()
-                .filter(n-> n.startsWith("신")) //신으로 시작하는 요소만을 필터
-                .forEach(n->System.out.println(n));
+                .filter(n -> n.startsWith("신")) //신으로 시작하는 요소만을 필터
+                .forEach(n -> System.out.println(n));
         System.out.println();
 
         list.stream()
                 .distinct() //먼저중복을 제ㅓㄱ하고
-                .filter(n->n.startsWith("신"))
-                .forEach(n->System.out.println(n));
+                .filter(n -> n.startsWith("신"))
+                .forEach(n -> System.out.println(n));
     }
+
     //요소 변환(매핑)
-    public void MapE(){
+    public void MapE() {
         List<Studentt> studentt = new ArrayList<>();
-        studentt.add(new Studentt("홍길동",99));
-        studentt.add(new Studentt("홍길동",23));
-        studentt.add(new Studentt("홍길동",23));
+        studentt.add(new Studentt("홍길동", 99));
+        studentt.add(new Studentt("홍길동", 23));
+        studentt.add(new Studentt("홍길동", 23));
 
         studentt.stream()
-                .mapToInt(s->s.getScore()) //데이터를 숫자로 mapToInt 숫자전용
+                .mapToInt(s -> s.getScore()) //데이터를 숫자로 mapToInt 숫자전용
                 .forEach(score -> System.out.println(score));
     }
+
     //요소를 복수 개의 요소로 변환
-    public void FlatMapint(){
-        List<String>list1 = new ArrayList<>();
+    public void FlatMapint() {
+        List<String> list1 = new ArrayList<>();
         list1.add("this is java");
         list1.add("i am best developer");
         list1.stream()
                 .flatMap(date -> Arrays.stream(date.split(" ")))//펼쳐서 하나로 만드는
-                .forEach(word->System.out.println(word));
+                .forEach(word -> System.out.println(word));
 
         System.out.println();
 
-        List<String>list2 = Arrays.asList("10,20,30,40,50");
+        List<String> list2 = Arrays.asList("10,20,30,40,50");
         list2.stream()
-                .flatMapToInt(data->{
-                    String[]strArr = data.split(",");
+                .flatMapToInt(data -> {
+                    String[] strArr = data.split(",");
                     int[] intArr = new int[strArr.length];
-                    for(int i = 0; i<strArr.length; i++){
+                    for (int i = 0; i < strArr.length; i++) {
                         intArr[i] = Integer.parseInt(strArr[i].trim());
                     }
                     return Arrays.stream(intArr);
                 })
-                .forEach(number->System.out.println(number));
+                .forEach(number -> System.out.println(number));
 
     }
+
     //요소정렬
-    public void Sorthing(){
+    public void Sorthing() {
         List<Stuudent> stuudentList = new ArrayList<>();
-        stuudentList.add(new Stuudent("홍길동",30));
-        stuudentList.add(new Stuudent("신용권",10));
-        stuudentList.add(new Stuudent("유미선",20));
+        stuudentList.add(new Stuudent("홍길동", 30));
+        stuudentList.add(new Stuudent("신용권", 10));
+        stuudentList.add(new Stuudent("유미선", 20));
         stuudentList.stream()
                 .sorted() //순서대로 정렬   오름차순
-                .forEach(s-> System.out.println(s.getName()+":"+s.getScore()));
+                .forEach(s -> System.out.println(s.getName() + ":" + s.getScore()));
         System.out.println();
         //Comparator를 이용한 정렬
 //        stuudentList.stream()
@@ -171,94 +185,98 @@ public class Ch17 {
 
         stuudentList.stream()
                 .sorted(Comparator.reverseOrder()) //내림차순
-                .forEach(s->System.out.println(s.getName()+":"+s.getScore()));
+                .forEach(s -> System.out.println(s.getName() + ":" + s.getScore()));
         //Comparator를 이용한 정렬
 //        stuudentList.stream()
 //                        .sorted((s1,s2)->Integer.compare(s2.getScore(),s1.getScore()))
 //                        .forEach(s-> System.out.println(s.getName()+":"+s.getScore()));
 //        System.out.println();
     }
+
     //요소를 하나씩 처리 (루핑)
-    public void Loopint(){
-        int[]intArr ={1,2,3,4,5};
+    public void Loopint() {
+        int[] intArr = {1, 2, 3, 4, 5};
         Arrays.stream(intArr)
-                .filter(a->a%2==0)
-                .peek(n->System.out.println(n)); //중간처리 최종처리가 없음으로 동작 하지않음
+                .filter(a -> a % 2 == 0)
+                .peek(n -> System.out.println(n)); //중간처리 최종처리가 없음으로 동작 하지않음
 
         int total = Arrays.stream(intArr)
-                .filter(a->a%2==0)
-                .peek(n->System.out.println(n))
+                .filter(a -> a % 2 == 0)
+                .peek(n -> System.out.println(n))
                 .sum();
-        System.out.println("총힙: " +total+"\n");
+        System.out.println("총힙: " + total + "\n");
 
         Arrays.stream(intArr)
-                .filter(a-> a%2==0)
-                .forEach(n->System.out.println(n));
+                .filter(a -> a % 2 == 0)
+                .forEach(n -> System.out.println(n));
     }
+
     //요소 조건만족 여부(매칭)
-    public void Matching(){
-        int[] intArr = {2,4,6};
+    public void Matching() {
+        int[] intArr = {2, 4, 6};
 
         boolean re = Arrays.stream(intArr)
-                .allMatch(a->a%2==0);
-        System.out.println("모두 2의 배수인가"+re);
+                .allMatch(a -> a % 2 == 0);
+        System.out.println("모두 2의 배수인가" + re);
 
         re = Arrays.stream(intArr)
-                .anyMatch(a-> a%3==0);
-        System.out.println("하나라도 3의 배수가 있는가?"+re);
+                .anyMatch(a -> a % 3 == 0);
+        System.out.println("하나라도 3의 배수가 있는가?" + re);
 
         re = Arrays.stream(intArr)
-                .noneMatch(a-> a%3==0);
-        System.out.println("3의 배수가 없는가?"+re);
+                .noneMatch(a -> a % 3 == 0);
+        System.out.println("3의 배수가 없는가?" + re);
     }
+
     //요소 기본 집계
-    public void Aggre(){
-        int[] arr ={1,2,3,4,5};
+    public void Aggre() {
+        int[] arr = {1, 2, 3, 4, 5};
 
         long count = Arrays.stream(arr)
-                .filter(n-> n%2==0)
+                .filter(n -> n % 2 == 0)
                 .count();
-        System.out.println("2의 배수 개수:"+count);
+        System.out.println("2의 배수 개수:" + count);
 
         long sum = Arrays.stream(arr)
-                .filter(n->n%2==0)
+                .filter(n -> n % 2 == 0)
                 .sum();
         System.out.println("2의 배수의 합");
 
         double avg = Arrays.stream(arr)
-                .filter(n->n%2==0)
+                .filter(n -> n % 2 == 0)
                 .average()
                 .getAsDouble();
-        System.out.println("2의 배수의 평균:"+avg);
+        System.out.println("2의 배수의 평균:" + avg);
 
         int max = Arrays.stream(arr)
-                .filter(n-> n%2==0)
+                .filter(n -> n % 2 == 0)
                 .max()
                 .getAsInt();
-        System.out.println("최대값:"+max);
+        System.out.println("최대값:" + max);
 
         int min = Arrays.stream(arr)
-                .filter(n-> n%2==0)
+                .filter(n -> n % 2 == 0)
                 .min()
                 .getAsInt();
-        System.out.println("최소값:"+min);
+        System.out.println("최소값:" + min);
 
         int first = Arrays.stream(arr)
-                .filter(n-> n%3==0)
+                .filter(n -> n % 3 == 0)
                 .findFirst()
                 .getAsInt();
-        System.out.println("첫 번째 3의 배수:"+first);
+        System.out.println("첫 번째 3의 배수:" + first);
     }
+
     //Optional클래스
-    public void Optional(){
-        List<Integer>list = new ArrayList<>();
+    public void Optional() {
+        List<Integer> list = new ArrayList<>();
 
         OptionalDouble optionalDouble = list.stream()
                 .mapToInt(Integer::intValue)
                 .average();
-        if(optionalDouble.isPresent()){
-            System.out.println("방법1_평균: "+optionalDouble.getAsDouble());
-        }else {
+        if (optionalDouble.isPresent()) {
+            System.out.println("방법1_평균: " + optionalDouble.getAsDouble());
+        } else {
             System.out.println("방법1_평균: 0.0");
         }
 
@@ -266,12 +284,120 @@ public class Ch17 {
                 .mapToInt(Integer::intValue)
                 .average()
                 .orElse(0.0);
-        System.out.println("방법2_평균: "+avg);
+        System.out.println("방법2_평균: " + avg);
 
         list.stream()
                 .mapToInt(Integer::intValue)
                 .average()
-                .
+                .ifPresent(a -> System.out.println("방법3_평균: " + a));
+
+    }
+
+    //요소커스텀 집계
+    public void Reduction() {
+        List<Sstudent> sstudentList = Arrays.asList(
+                new Sstudent("홍길동", 10),
+                new Sstudent("신용권", 66),
+                new Sstudent("집갈래", 23)
+        );
+
+        int sum1 = sstudentList.stream()
+                .mapToInt(Sstudent::getScore)
+                .sum();
+        int sum2 = sstudentList.stream()
+                .map(Sstudent::getScore)
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println("sum1:" + sum1);
+        System.out.println("sum2:" + sum2);
+
+    }
+
+    //요소 수집
+    public void CollectE() {
+        List<eotjfs> eotjfsList = new ArrayList<>();
+        eotjfsList.add(new eotjfs("홍길동", "남", 70));
+        eotjfsList.add(new eotjfs("김수영", "여", 11));
+        eotjfsList.add(new eotjfs("김자바", "남", 88));
+        eotjfsList.add(new eotjfs("오해영", "여", 78));
+
+        List<eotjfs> male = eotjfsList.stream()
+                .filter(s -> s.getSex().equals("남"))
+                .toList();
+        male.stream()
+                .forEach(s -> System.out.println(s.getName()));
+
+        System.out.println();
+
+        Map<String, Integer> map = eotjfsList.stream()
+                .collect(
+                        Collectors.toMap(
+                                s -> s.getName(),
+                                s -> s.getScore()
+
+                        )
+                );
+        System.out.println(map);
+
+
+    }
+    //요소 그룹핑
+    public void CollectEefe() {
+        List<eotjfs> eotjfsList = new ArrayList<>();
+        eotjfsList.add(new eotjfs("홍길동", "남", 70));
+        eotjfsList.add(new eotjfs("김수영", "여", 11));
+        eotjfsList.add(new eotjfs("김자바", "남", 88));
+        eotjfsList.add(new eotjfs("오해영", "여", 78));
+
+
+        Map<String, List<eotjfs>> map = eotjfsList.stream()
+                .collect(
+                        Collectors.groupingBy(s->s.getSex())
+                );
+
+        List<eotjfs> maleList = map.get("남");
+        maleList.stream().forEach(s->System.out.println(s.getName()));
+        System.out.println();
+
+        List<eotjfs> femaleList = map.get("여");
+        femaleList.stream().forEach(s->System.out.println(s.getName()));
+
+    }
+
+    //요소병렬처리
+    public void ParallelE(){
+        Random random = new Random();
+
+        List<Integer>scorse = new ArrayList<>();
+        for(int i=0; i<1000000;i++){
+            scorse.add(random.nextInt(101));
+        }
+
+        double avg = 0.0;
+        long startTime = 0;
+        long endTime = 0;
+        long time = 0;
+
+        Stream<Integer> stream =scorse.stream();
+        startTime = System.nanoTime();
+        avg = stream
+                .mapToInt(i->i.intValue())
+                .average()
+                .getAsDouble();
+        endTime =System.nanoTime();
+        time = endTime - startTime;
+        System.out.println("avg: "+avg+",일반 스트림 처리 시간: "+time+"ns");
+
+        Stream<Integer>parer = scorse.parallelStream();
+        startTime =System.nanoTime();
+        avg =parer
+                .mapToInt(i-> i.intValue())
+                .average()
+                .getAsDouble();
+        endTime = System.nanoTime();
+
+        time = endTime - startTime;
+        System.out.println("avg: "+avg+",병렬 스트림 처리 시간: "+time+"ns");
     }
 
 }
