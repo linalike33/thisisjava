d341e0abc2541491ce2a3e9d4936327e07ab4fa4
nahyun <linalike33@gmail.com>
2026-03-20 21:43:57 +0900

    네트워크 인텔 연결하다가 말았음 마저 연결

---
+++ ch20/src/main/java/com/mjc813/ch20/Ch20Application.java
@@ -0,0 +1,13 @@
+package com.mjc813.ch20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch20Application.class, args);
+    }
+
+}
