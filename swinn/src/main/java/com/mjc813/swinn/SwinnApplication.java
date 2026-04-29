cd9c41ce4829da0e420e7090f8b174f44ecdbd76
nahyun <linalike33@gmail.com>
2026-04-29 18:09:51 +0900

    swin 프로젝트

---
+++ swinn/src/main/java/com/mjc813/swinn/SwinnApplication.java
@@ -0,0 +1,13 @@
+package com.mjc813.swinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwinnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwinnApplication.class, args);
+    }
+
+}
---
+++ swinn/src/main/java/com/mjc813/swinn/models/ComeomResponseCode.java
@@ -0,0 +1,4 @@
+package com.mjc813.swinn.models;
+
+public class ComeomResponseCode {
+}
